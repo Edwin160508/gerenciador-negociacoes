0e7599f2e329d5965d9583465e4061cd5e6e3b92
Edwin Lima <edwin.limaa@gmail.com>
2017-06-02 16:27:40 -0300

    Solucionando compatibilidade com navegador Microsoft Edge

--- client/js/app/models/Mensagem.js
+++ client/js/app/models/Mensagem.js
@@ -1,6 +1,10 @@
 class Mensagem {
-    constructor(texto=''){
+    /*constructor(texto=''){
         this._texto = texto;
+    }*/
+    // Prestando suporte ao navegador Edger 13 que não suporta recebimento de parametro Opcional
+    constructor(texto){
+        this._texto = texto || '';//se texto for undefined, o valor passa a ser '' -> String em branco
     }
 
     get texto(){
